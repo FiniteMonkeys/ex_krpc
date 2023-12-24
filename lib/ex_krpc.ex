a030eab89d4f140e531a487fb8c1ee88b5e5a186
Kris Cottingham <craig@cottingham.net>
2023-12-24 16:02:46 -0600

    chore: clean up credo warnings

--- lib/ex_krpc.ex
+++ lib/ex_krpc.ex
@@ -2,17 +2,4 @@ defmodule ExKrpc do
   @moduledoc """
   Documentation for `ExKrpc`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExKrpc.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
